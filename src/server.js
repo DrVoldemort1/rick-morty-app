2fc33f5b778e5823e3d6151c0e8a1a83734b81f7
VJGM <vgonzalez33@alumnos.uaq.mx>
2021-11-10 20:24:12 -0600

    Revert "Done for heroku"
    This reverts commit 512752a9d1a58109dee9294b27999e79ca6cc306.

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const app = express();
-
-app.use(express.static(__dirname+'/dist/rick-morty-app'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/rick-morty-app/index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
